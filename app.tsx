c2807df99348b16228b2069d6b435f7b8afd1f26
slyobard <slyobard@mercator-ocean.fr>
2025-04-15 13:45:23 +0200

    tests

--- app.tsx
+++ app.tsx
@@ -1,10 +1,14 @@
 import { GeoArrowPolygonLayer } from "@geoarrow/deck.gl-layers";
+import PauseCircleIcon from "@mui/icons-material/PauseCircle";
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import { Box, Button, Slider } from "@mui/material";
 import * as arrow from "apache-arrow";
 import * as d3 from "d3";
-import DeckGL, { Layer, MapViewState, PickingInfo } from "deck.gl";
-import React, { useEffect, useState } from "react";
+import DeckGL, { Layer, MapView, MapViewState, PickingInfo } from "deck.gl";
+import React, { useEffect, useRef, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { StaticMap } from "react-map-gl";
+import "./App.css";
 import {
   getAnonymousS3Client,
   getObjectByteArray,
@@ -16,7 +20,7 @@ const S3_REGION = "waw3-1";
 const S3_BUCKET_NAME = "project-chlorophyll";
 const S3_PREFIX = "TESTS_SIMON";
 
-const ANIMATION_TIMEOUT = 1000;
+const ANIMATION_TIMEOUT = 2000;
 
 const INITIAL_VIEW_STATE: MapViewState = {
   latitude: 20,
@@ -24,11 +28,12 @@ const INITIAL_VIEW_STATE: MapViewState = {
   zoom: 2,
   bearing: 0,
   pitch: 0,
+  minZoom: 2,
 };
 
-const colorLow = d3.color("#2C353B")
-const colorHigh = d3.color("#5FD490")
-const COLOR_GRADIENT = d3.scaleLinear([0, 1], [colorLow, colorHigh])
+const colorLow = d3.color("#2C353B");
+const colorHigh = d3.color("#5FD490");
+const COLOR_GRADIENT = d3.scaleLinear([0, 1], [colorLow, colorHigh]);
 
 const s3Client = getAnonymousS3Client(S3_ENDPOINT, S3_REGION);
 
@@ -36,11 +41,12 @@ const MAP_STYLE =
   "https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json";
 
 // 04APR_CHL5D_6MFORECAST_norm-2025-04-01.feather
+const dateRegExp = "\\d{4}-\\d{2}-\\d{2}";
 const fileRegExp = new RegExp(
-  `^${S3_PREFIX}/04APR_CHL5D_6MFORECAST_norm-\\d{4}-\\d{2}-\\d{2}.feather$`,
+  `^${S3_PREFIX}/04APR_CHL5D_6MFORECAST_norm-${dateRegExp}.feather$`,
 );
 
-function Root() {
+function App() {
   const onClick = (info: PickingInfo) => {
     if (info.object) {
       console.log(JSON.stringify(info.object.toJSON()));
@@ -48,6 +54,10 @@ function Root() {
   };
 
   const [table, setTable] = useState<arrow.Table | undefined>(undefined);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [s3ObjectKeys, setS3ObjectKeys] = useState<string[]>([]);
+  const [s3ObjectCurrentIndex, setS3ObjectCurrentIndex] = useState<number>(0);
+  const timeoutRef = useRef<NodeJS.Timeout>();
 
   useEffect(() => {
     listObjectsWithPrefix(s3Client, S3_BUCKET_NAME, S3_PREFIX).then(
@@ -55,22 +65,51 @@ function Root() {
         const filteredObjects = objects.filter((object) =>
           object.match(fileRegExp),
         );
-        for (let index = 0; index < filteredObjects.length; index++) {
-          const featherFileS3ObjectKey = filteredObjects[index];
-          const data = await getObjectByteArray(
-            s3Client,
-            S3_BUCKET_NAME,
-            featherFileS3ObjectKey,
-          );
-          const table = arrow.tableFromIPC(data);
-          setTable(table);
-          await new Promise((resolve) =>
-            setTimeout(resolve, ANIMATION_TIMEOUT),
-          );
-        }
+        setS3ObjectKeys(filteredObjects.sort());
       },
     );
   }, []);
+
+  const handleChangeDate = (newIndex: number) => {
+    setS3ObjectCurrentIndex(newIndex);
+  };
+
+  const handlePlayPause = async (newValue: boolean) => {
+    setIsPlaying(newValue);
+  };
+
+  const startLoop = (index: number) => {
+    timeoutRef.current = setTimeout(() => {
+      let newIndex = index + 1;
+      if (newIndex < s3ObjectKeys.length) {
+        setS3ObjectCurrentIndex(newIndex);
+        startLoop(newIndex);
+      } else {
+        setIsPlaying(false); // stop animation at the end
+      }
+    }, ANIMATION_TIMEOUT);
+  };
+
+  useEffect(() => {
+    if (isPlaying) {
+      startLoop(s3ObjectCurrentIndex);
+    }
+    return () => clearTimeout(timeoutRef.current);
+  }, [isPlaying]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const key = s3ObjectKeys[s3ObjectCurrentIndex];
+      try {
+        const data = await getObjectByteArray(s3Client, S3_BUCKET_NAME, key);
+        const table = arrow.tableFromIPC(data);
+        setTable(table);
+      } catch (err) {
+        console.error(`Error fetching ${key}:`, err);
+      }
+    };
+    fetchData();
+  }, [s3ObjectKeys, s3ObjectCurrentIndex]);
 
   const layers: Layer[] = [];
 
@@ -89,25 +128,57 @@ function Root() {
         getFillColor: ({ index, data, target }) => {
           const recordBatch = data.data;
           const row = recordBatch.get(index)!;
-          const color = d3.color(COLOR_GRADIENT(row["CHL"])!).rgb()
-          return [color.r, color.g, color.b]
+          const color = d3.color(COLOR_GRADIENT(row["CHL"])!).rgb();
+          return [color.r, color.g, color.b];
         },
         _normalize: false,
       }),
     );
 
+  function valuetext(index: number) {
+    if (s3ObjectKeys.length > 0) {
+      const s3ObjectKey = s3ObjectKeys[index];
+      return s3ObjectKey.match(dateRegExp)![0];
+    } else {
+      return "";
+    }
+  }
+
   return (
-    <DeckGL
-      initialViewState={INITIAL_VIEW_STATE}
-      controller
-      layers={layers}
-      onClick={onClick}
-    >
-      <StaticMap mapStyle={MAP_STYLE} />
-    </DeckGL>
+    <div className="my-app">
+      <DeckGL
+        initialViewState={INITIAL_VIEW_STATE}
+        controller
+        layers={layers}
+        onClick={onClick}
+        views={
+          new MapView({
+            repeat: true,
+          })
+        }
+      >
+        <StaticMap mapStyle={MAP_STYLE} />
+      </DeckGL>
+      <Box>
+        <Slider
+          value={s3ObjectCurrentIndex}
+          onChange={(_event, index) => handleChangeDate(index)}
+          step={1}
+          min={0}
+          max={s3ObjectKeys.length - 1}
+        />
+        <Button
+          variant="text"
+          startIcon={isPlaying ? <PauseCircleIcon /> : <PlayCircleIcon />}
+          onClick={() => handlePlayPause(!isPlaying)}
+        >
+          {valuetext(s3ObjectCurrentIndex)}
+        </Button>
+      </Box>
+    </div>
   );
 }
 
 /* global document */
 const container = document.body.appendChild(document.createElement("div"));
-createRoot(container).render(<Root />);
+createRoot(container).render(<App />);
