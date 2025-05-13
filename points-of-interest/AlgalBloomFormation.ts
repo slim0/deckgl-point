eb10cb3902f567f845d5f5d894e82a73dadd54da
slyobard <slyobard@mercator-ocean.fr>
2025-05-13 09:35:10 +0200

    chore: Add local image for Algal Bloom Formation point of intereset

--- points-of-interest/AlgalBloomFormation.ts
+++ points-of-interest/AlgalBloomFormation.ts
@@ -1,3 +1,4 @@
+import AlgalBloomFormationImage from "../img/AlgalBloomFormation.jpg";
 import { PointOfInterest } from "./common";
 
 export const algalBloomFormation: PointOfInterest = {
@@ -10,8 +11,7 @@ export const algalBloomFormation: PointOfInterest = {
       "Wang, M., et al. (2019). The great Atlantic Sargassum belt. Science, 365(6448), 83-87. https://doi.org/10.1126/science.aaw7912",
       "Johns, E. M., et al (2020). The establishment of a pelagic Sargassum population in the tropical Atlantic: Implications for the Great Atlantic Sargassum Belt. Progress in Oceanography, 182, 102269. https://doi.org/10.1016/j.pocean.2020.102269",
     ],
-    preview:
-      "https://upload.wikimedia.org/wikipedia/commons/6/61/Sargassum_sp._%28Sargasse%29.jpg?uselang=fr",
+    preview: AlgalBloomFormationImage,
   },
   geometry: {
     coordinates: [-37.533889, 10.9835],
